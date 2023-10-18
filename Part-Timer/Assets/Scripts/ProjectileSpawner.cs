82e10db9d48f333f47f58da3dd3389957ce7daf0
Savior001 <ptg426@my.utsa.edu>
2023-10-17 23:07:47 -0500

    Completed prototype

--- Part-Timer/Assets/Scripts/ProjectileSpawner.cs
+++ Part-Timer/Assets/Scripts/ProjectileSpawner.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class ProjectileSpawner : MonoBehaviour {
     [SerializeField] GameObject projectilePrefab;
     [SerializeField] GameObject weaponObject;
+    [SerializeField] AudioClip stapleClip;
     [SerializeField] public float power = 1f;
     [SerializeField] float speed = 5f;
 
     public void ShootStaple(Vector3 targetPosition) {
+        GetComponent<AudioSource>().PlayOneShot(stapleClip);
         Rigidbody2D newProjectileRB = Instantiate(projectilePrefab, transform.position, Quaternion.identity).GetComponent<Rigidbody2D>();
         targetPosition.z = 0;
         Vector3 direction = (targetPosition - transform.position).normalized;
