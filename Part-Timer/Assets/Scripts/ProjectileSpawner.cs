b5f3c6220ae05d92edd7a68f4097021cae8e705f
Savior001 <ptg426@my.utsa.edu>
2023-11-30 21:28:23 -0600

    adding object pooling for staples

--- Part-Timer/Assets/Scripts/ProjectileSpawner.cs
+++ Part-Timer/Assets/Scripts/ProjectileSpawner.cs
@@ -7,18 +7,31 @@ public class ProjectileSpawner : MonoBehaviour {
     [SerializeField] GameObject playerObject;
     [SerializeField] GameObject weaponObject;
     [SerializeField] AudioClip stapleClip;
+    [SerializeField] int staplesPoolCap = 50;
+    public List<Rigidbody2D> staplesPool;
     public GameInfoSO gameInfoSO;
     float speed = 5f;
     float power = 1f;
 
-    void FixedUpdate() {
+    void Start() {
+        staplesPool = new List<Rigidbody2D>();
     }
 
     public void ShootStaple(Vector3 targetPosition) {
         speed = gameInfoSO.playerSpeed;
         power = gameInfoSO.playerPower;
         GetComponent<AudioSource>().PlayOneShot(stapleClip);
-        Rigidbody2D newProjectileRB = Instantiate(projectilePrefab, transform.position, Quaternion.identity).GetComponent<Rigidbody2D>();
+        Rigidbody2D newProjectileRB;// = Instantiate(projectilePrefab, transform.position, Quaternion.identity).GetComponent<Rigidbody2D>();
+        
+        if (staplesPool.Count > staplesPoolCap) {
+            staplesPool.RemoveAt(0);
+            newProjectileRB = staplesPool[0];
+            newProjectileRB.transform.position = transform.position;
+        } else {
+            newProjectileRB = Instantiate(projectilePrefab, transform.position, Quaternion.identity).GetComponent<Rigidbody2D>();
+        }
+
+        staplesPool.Add(newProjectileRB);
         targetPosition.z = 0;
         Vector3 direction = (targetPosition - transform.position).normalized;
         // Vector3 direction = (targetPosition - transform.position).normalized;
